d57af27727ebc09e363b8f0f1b7baeba9b399568
pavan ralla <pavankalyanralla99@gmail.com>
2025-08-10 09:13:59 +0530

    assignment1 completed.

---
+++ pavan_kalyan_ralla_Python_Assignment1.py
@@ -0,0 +1,10 @@
+"""
+1.Explain one key difference in Python installation on Windows, Mac, and Linux.
+A.Windows → Python is not preinstalled. You must download it from python.org or use the Microsoft Store.
+           During installation, you also need to check "Add Python to PATH" manually.
+Mac → macOS comes with an older version of Python 2.x preinstalled (mainly for system tools).
+           You usually install a newer Python 3.x version via Homebrew or python.org.
+Linux → Most Linux distros already have Python 3 preinstalled and integrated into the system.
+           You can install/update it easily via your package manager (apt, dnf, etc.)
+
+"""
